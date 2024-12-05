6d8c503a386dd8b4b4460ddbe5220833b0e64e24
air005831 <130114913+air005831@users.noreply.github.com>
2024-12-05 15:37:35 +0800

    更新 main5.c

---
+++ EX3/main5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int ParkTime = 119;
+    if(ParkTime <= 30){
+        printf("免費");
+    }
+    else if(ParkTime >= 240){
+        printf("240元");
+    }
+    else{
+        if(i%30){
+            printf("%d 元", int h = ((i/30)+1)*30);
+        }
+        else{
+            printf("%d", i);
+        }
+    }
+    return 0;
+}
