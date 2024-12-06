6e79bdc62177025adc54c51745c10d53bc8d8ff9
air005831 <130114913+air005831@users.noreply.github.com>
2024-12-06 21:04:08 +0800

    Create main3.c

---
+++ hw5/main3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 1, y = 1;
+    for(int i = 1 ; ; i++){
+        x = i;
+        if(i == 10 ){
+            printf("\n");
+            i = 0;
+            y += 1;
+        }
+        else if(x*y == 81){
+            printf("%d * %d = %d\t", x, y, x*y);
+            break;
+        }
+        else{
+            printf("%d * %d = %d\t", x, y, x*y);
+        }
+        
+        //if == 10 : x = 1 y += 1
+        //if x*y == 81
+    }
+
+    return 0;
+}
